0aff90b886ef95dc88ac1ddd6acc8d54a64994c4
Omkar <onkarrkhade3@gmail.com>
2023-10-30 21:39:22 +0530

    commit 4.1

--- ingest.py
+++ ingest.py
@@ -1,44 +1,36 @@
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings 
 from langchain.vectorstores import FAISS
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
+#from langchain.document_loaders import TextFilesLoader  
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import TextLoader
 
-DATA_PATH = 'translated_pdfs'
+DATA_PATH = 'translated_pdfs\AFFAIRE A.txt'
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-import chardet
-
-# Create vector database
 def create_vector_db():
-    loader = DirectoryLoader('./translated_pdfs', glob="**/*.txt", loader_cls=TextLoader)
 
-    documents = []
-    for document in loader.load():
-        try:
-            with open(document.filepath, 'rb') as file:
-                rawdata = file.read()
-                result = chardet.detect(rawdata)
-                file_encoding = result['encoding']
-                text = rawdata.decode(file_encoding)
-                document.text = text
-                documents.append(document)
-        except Exception as e:
-            print(f"Error loading {document.filepath}: {e}")
+  loader = TextLoader(DATA_PATH, encoding='utf-8')
 
-    print("Documents loaded successfully")
+  try:
+    documents = loader.load()
+  except FileNotFoundError:
+    print("Could not access translated_pdfs folder")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
-    texts = text_splitter.split_documents(documents)
-    print("Text splitting done successfully")
+  #documents = loader.load()
 
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
-    print("Embeddings model loaded successfully")
+  print("Documents loaded")
 
-    db = FAISS.from_documents(texts, embeddings)
-    db.save_local(DB_FAISS_PATH)
-    print("Databases saved successfully")
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+
+  texts = text_splitter.split_documents(documents)
+
+  embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+
+  db = FAISS.from_documents(texts, embeddings)
+
+  db.save_local(DB_FAISS_PATH)
+
+  print("Vector DB created")
 
 if __name__ == "__main__":
-    create_vector_db()
-
+   create_vector_db()
